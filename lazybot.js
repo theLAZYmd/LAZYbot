5253ae7ac3b7113eeb29f85afb35ada0397e7ea7
lazy <lazymeep+github@gmail.com>
2018-03-11 17:35:57 +0000

    added native mf and tf support fixed || bug
    added native mf and tf support fixed || bug

--- lazybot.js
+++ lazybot.js
@@ -11,22 +11,53 @@ const prefix = config.prefix;
 
 client.on("message", (message) => {
 
-  if (!message.content.startsWith("/r/" || "r/") || message.author.bot) return;
+  if (!(message.content.startsWith("/r/") || message.content.startsWith("r/")) || message.author.bot) return;
 
   if(message.content.startsWith("/r/")) {
     message.channel.send({embed: {
       color: 53380,
-      description: `[**${message.content}**](http://www.reddit.com/${message.content})`
+      description: `[**${message.content}**](http://www.reddit.com${message.content})`
     }});
   } else
 
   if(message.content.startsWith("r/")) {
     message.channel.send({embed: {
       color: 53380,
-      description: `[**${message.content}**](http://www.reddit.com/${message.content})`
+      description: `[**/${message.content}**](http://www.reddit.com/${message.content})`
     }});
   }
   
+});
+
+client.on("message", (message) => {
+
+  if (!(message.content.startsWith(".mf") || message.content.startsWith(".tf")) || message.author.bot) return;
+
+  const args = message.content.slice(1).trim().split(/ +/g);
+  const command = args.shift().toLowerCase();
+  
+  if (command === "mf") {
+
+  let [games,time,increment] = args
+  message.channel.send({embed: {
+    title: "House Match Reward",
+    color: 53380,
+    description: Math.floor(7/12 * parseInt(games) * (parseInt(time) + 2/3 * parseInt(increment))) + " :cherry_blossom:"
+  }});
+
+  } else
+
+  if (command === "tf") {
+
+  let [tgames,ttime,tincrement] = args
+  message.channel.send({embed: {
+    title: "House Tournament Reward",
+    color: 53380,
+    description: Math.floor(1/10 * parseInt(tgames) * (parseInt(ttime) + 2/3 * parseInt(tincrement))) + " :cherry_blossom:"
+  }});
+
+  }
+
 });
 
 client.on("message", (message) => {
@@ -41,7 +72,7 @@ client.on("message", (message) => {
     message.channel.send(`Hello **${message.author.username}**, I see you're a **${age}** year old **${sex}** from **${location}**.`);
   } else
 
-  if (command === "prefix") {
+  if (command === "prefix" || command === "lazybotprefix") {
 
     if(message.author.id !== config.ownerID) return;
 
@@ -73,7 +104,7 @@ client.on("message", (message) => {
     message.channel.send("theLAZYmd#2353");
   } else
 
-  if(command === "help") {
+  if(command === "help || help") {
     message.channel.send("This is a pretty basic bot, there isn't much it can help you with.");
   } else
 
