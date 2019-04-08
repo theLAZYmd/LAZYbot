5a05a9a9381fa9abeb508875213909f288810bc9
theLAZYmd <lazymeep+github@gmail.com>
2019-04-08 01:48:24 +0100

    Revamped tracker to Track instance with success on link

--- src/modules/Chess/tracker/tracker.js
+++ src/modules/Chess/tracker/tracker.js
@@ -14,23 +14,116 @@ const Lichess = new client();
  * @typedef {object} TrackInput
  * @property {DBuser} dbuser
  * @property {Object[]} sources
- * @property {Object} source
  * @property {string} username
  */
 class Track {
 
-    constructor({argsInfo, dbuser, source, username}) {
+    constructor(argsInfo, dbuser) {
+        this.successes = [];
+        this.errors = [];
         this.argsInfo = argsInfo;
         this.dbuser = dbuser;
-        this.source = source;
-        this.username = username;
-        this.successfulUpdates = [];
-        this.errors = [];
     }
 
     /**
-     * Assigns parsed data from a chess website to the properties of the user in the database
-     * @param {LichessUser|ChesscomUser} parsedData 
+     * Returns whether this Track instance was initiated by a command (true) or automatically (false)
+     * @name Track#command
+     * @type {Boolean}
+     */
+    get command() {
+        this._command = this.argsInfo.command
+        return Boolean(this._command);
+    }
+    
+    /**
+     * Returns the list of sources for which the DBuser can validly be updated
+     * @name Track#sources
+     * @type {Array}
+     */
+    get sources() {
+        if (this._sources) return this._sources;
+        return this._sources = Object.values(config.sources).filter(s => this.dbuser[s.key]);
+    }
+
+    /**
+     * Sets the source property of the Track instance to a source object ready to grab data from
+     * @name Track#source
+     * @param {Object} source 
+     */
+    setSource(source) {
+        this.source = source;
+        return this;
+    }
+
+    /**
+     * Sets the username property of the Track instance to a given username
+     * @name Track#username
+     * @param {string} username 
+     */
+    setUsername(username) {
+        username = username.replace(/[^\w-]+/g, "");			
+        if (!username.match(/[a-z0-9][\w-]*[a-z0-9]/i)) throw new Error("Invalid syntax for a username.");
+        for (let s of this.sources) {
+            for (let u in this.dbuser[s.key]) {
+                if (u.toLowerCase() === username.toLowerCase()) {
+                    username = u;
+                }
+            }
+        }
+        this.username = username;
+        return this;
+    }
+
+    /**
+     * Sets a message to be edited in a command-called update embed
+     * @name Track#message
+     * @param {Message} message
+     */
+    setMessage(message) {
+        this.message = message;
+    }
+
+    setError(e) {
+        this.errors.push([this.source.key, this.username, e]);
+        return this;
+    }
+
+    setSuccess() {
+        this.successes.push([this.source.key, this.username]);
+        return this;
+    }
+
+    /**
+     * Edits a running embed for an update command
+     * @private
+     */
+    async edit() {
+        if (this.command) this.argsInfo.Output.editor({
+            description: `Updating user ${this.dbuser.username}... on **${this.source.name}**`
+        }, this.message);
+    }
+    
+    /**
+     * Logs the list of accounts that have been updated to the console
+     * @private
+     */
+    async log() {
+        if (this.successes.length === 0) return;
+        Logger.log(["Tracker", "update", "auto", "[" + [this.dbuser.username, ...(this.successes.map(s => `${s[0]}: ${s[1]}`))].join(", ") + "]"]);
+    }
+
+    async getData() {
+        let parsedData = /lichess|bughousetest/.test(this.source.key) ? await Lichess.users.get(this.username) : await (new require('chesscom.js'))(await Tracker.request(this), this);
+        if (!parsedData) throw "No response received";
+        this.assign(parsedData);
+        this.setSuccess();
+        return this;
+    }
+    
+    /**
+     * Assigns parsed data from a chess website to the properties of the user in the database. Follows on from getting data
+     * @param {LichessUser|ChesscomUser} parsedData
+     * @private
      */
     assign(parsedData) {
         let account = this.dbuser[this.source.key] || {    _main: parsedData.username      };
@@ -41,63 +134,50 @@ class Track {
                 else if (account["_" + prop]) delete account["_" + prop]; //keeps it in sync - if excess data, delete it
             }
         }
-        this.dbuser[data.source.key] = account;
+        this.dbuser[this.source.key] = account;
         return this;
     }
-
+    
+    /**
+     * Sets the modified data for a DBuser to the database
+     * @private
+     */
+    async setData() {
+        if (this.dbuser.lastupdate) delete this.dbuser.lastupdate;
+        this.dbuser.lastUpdate = Date.now();
+        DBuser.setData(this.dbuser);
+        this.log();        
+        if (this.command) this.output();
+        return this;
+    }
+    
+    /**
+     * Outputs the result of a new link or update
+     * @private
+     */
     async output() {
         try {
             const whitespace = "      \u200B";
-            let errors = "";
-            let embed = new Embed()
-                .setColor(this.argsInfo.server.colors.ratings);
-            for (let source of this.sources) {
-                for (let account of (this.command === "update" ? Object.keys(this.dbuser[source.key]) : [this.username])) try {
-                    if (account.startsWith("_")) continue;
-                    if (!data.successfulupdates.includes(source.key)) errors += this.argsInfo.Search.emojis.get(source.key) + " Couldn't " + (this.command === "update" ? "update '" : "link to '") + account + "' on " + source.name + (data.errors[source.key] ? ": " + data.errors[source.key] : ".") + "\n";
+            let errors = [];
+            let embed = new Embed().setColor(this.argsInfo.server.colors.ratings);
+            let sources = this._command === "update" ? this.sources : [this.source];
+            for (let source of sources) {                
+                let accounts = this._command === "update" ? Object.keys(this.dbuser[source.key]) : [this.username];
+                for (let account of accounts) if (!account.startsWith("_")) {
+                    if (this.successes.every(([s, a]) => s !== source.key && a !== account)) errors.push(`${this.argsInfo.Search.emojis.get(source.key)} Couldn't ${this._command === "update" ? "update" : "link to"} '${account}' on ${source.name}${this.errors.find(([s, a]) => s === source.key && a === account) ? `: ${this.errors[source.key]}` : "."}`);
                     else embed.addField(
-                        `${this.argsInfo.Search.emojis.get(source.key)} ${this.command === "update" ? "Updated" : `Linked ${this.user.username} to`} '${account}'`,
-                        Parse.profile(data.dbuser, source, account) + "\nCurrent highest rating is **" + data.dbuser[source.key][account].maxRating + "**" + whitespace + "\n" + Parse.ratingData(data.dbuser, source, account),
+                        `${this.argsInfo.Search.emojis.get(source.key)} ${this.command === "update" ? "Updated" : `Linked ${this.dbuser.username} to`} '${account}'`,
+                        Parse.profile(this.dbuser, source, account) + "\nCurrent highest rating is **" + this.dbuser[source.key][account].maxRating + "**" + whitespace + "\n" + Parse.ratingData(this.dbuser, source, account),
                         true
                     )
-                } catch (e) {
-                    if (e) throw e;
                 }
             }
-            if (embed.fields.length > 0) this.Output[this.command === "update" ? "editor" : "sender"](embed, this.msg);
-            if (errors) throw errors;
+            if (embed.fields.length > 0) this.argsInfo.Output[this.command === "update" ? "editor" : "sender"](embed, this.msg);
+            if (errors.length) throw errors.join("\n");
         } catch (e) {
             if (e) this.argsInfo.Output.onError(e);
-        }
-    }
-
-    setError() {
-        this.errors.push([this.source.key, this.username]);
+        }        
         return this;
-    }
-
-    setSuccess() {
-        this.successfulUpdates.push([this.source.key, this.username]);
-        return this;
-    }
-
-    async setData () {
-        DBuser.setData(this.dbuser);
-    }
-
-    set username (username) {
-        username = username.replace(/[^\w-]+/g, "");			
-        if (!username.match(/[a-z0-9][\w-]*[a-z0-9]/i)) throw new Error("Invalid syntax for a username.");
-        for (let u in this.dbuser[this.sources[0].key]) {
-            if (u.toLowerCase() === username.toLowerCase()) {
-                username = u;
-            }
-        }
-    }
-
-    get sources() {
-        if (this._sources) return this._sources;        
-        return this._sources = Object.values(config.sources).filter(s => this.dbuser[s.key]);
     }
 
 }
@@ -108,25 +188,6 @@ class Tracker extends Parse {
 		super(message);
 		if (guildID) this.message.guild = this.Search.guilds.byID(guildID);
 	}
-
-    /**
-     * @private
-     */
-	get output() {
-		let OutputConstructor = require("./output.js");
-		return new OutputConstructor(this.message, this.msg);
-	}
-
-    /**
-     * Grabs 
-     * @private
-     */
-	async updateCycle () {
-		let dbuser = this.nextUpdate();
-        Tracker.lastDBuser = dbuser;
-		if (!dbuser) return Logger.error("All users are up to date.");
-        this.track(new Track({argsInfo: this, dbuser}));
-    }
 
     /**
      * Links a user's Discord account to an account on a specific API website, so-far support for Lichess.org and Chess.com
@@ -150,63 +211,54 @@ class Tracker extends Parse {
                     dbuser = DBuser.getUser(user);
             }
             let source = Object.values(config.sources).find(s => s.key === command.toLowerCase().replace(/\./g, ""));
-            let data = new Track({argsInfo: this, dbuser, source, username});
+            if (dbuser[source.key] && dbuser[source.key][username]) throw "Already linked account.";
+            let data = new Track(this, dbuser);
+            await data.setSource(source).setUsername(username).getData();
+            data.setData();
 		} catch (e) {
 			if (e) this.Output.onError(e);
 		}
     }
-    
-    /**
-     * Begins a new process to link accounts.
-     * @param {TrackData} data 
-     * @private
-     */
-    async track(data) {
-		try {
-			for (let source of data.sources) try {
-				data.source = source;
-				if (this.command && data.username && this.command !== "update") {
-					if (data.dbuser[data.source.key] && data.dbuser[data.source.key][data.username]) throw `Already linked account.`;
-					data = await Tracker.handle(data, this.msg);
-				} else {
-					for (let account in data.dbuser[data.source.key]) {
-						if (account.startsWith("_") || !data.dbuser[data.source.key].hasOwnProperty(account)) continue;
-						data.username = account;
-						if (this.command) this.Output.editor({
-							"description": "Updating user " + data.dbuser.username + "... on **" + data.source.name + "**"
-						}, this.msg);
-						data = await Tracker.handle(data, this.msg);
-						data.username = "";
-					}
-				}
-			} catch (e) {
-				if (e) data.errors[data.source.key] = e;
-			}
-			data.dbuser.lastupdate = Date.now(); //Mark the update time
-			DBuser.setData(data.dbuser); //set it
-			if (data.successfulupdates.length > 0) Logger.log(["Tracker", "update", "auto", "[" + [data.dbuser.username, ...data.successfulupdates].join(", ") + "]"]);
-			if (this.command) this.output.track(data, this.msg);
-		} catch (e) {
-			if (e) throw e;
-		}
-    }
 
-    async update(user = this.user) {
+    /**
+     * 
+     * @param {*} user 
+     */
+    async update(dbuser = this.dbuser) {
         try {
             if (this.argument) {
                 if (!this.Permissions.role('admin', this)) throw this.Permissions.output('role');
                 user = this.Search.users.get(this.argument);
                 if (!user) throw new Error("Couldn't find user **" + this.argument + "** in this server");
+                dbuser = DBuser.getUser(user);
             }
-			let dbuser = DBuser.getUser(user);
-            if (dbuser.username !== this.user.tag) dbuser.username = user.tag;
-            let data = new Track({argsInfo: this, dbuser});
-			if (data.sources.length === 0) throw `No linked accounts found.\nPlease link an account to your profile through \`${this.generic}lichess\`, \`${this.generic}chess.com\`, or \`${this.generic}bughousetest\`.";
-			if (this.command) this.msg = await this.Output.generic("Updating user " + dbuser.username + "...");
-			await this.track({dbuser, sources});
+            if (this.user && dbuser.username !== this.user.tag) dbuser.username = user.tag;            
+            let data = new Track(this, dbuser);
+            try {
+                if (data.sources.length === 0) throw `No linked accounts found.\nPlease link an account to your profile through \`${this.generic}lichess\`, \`${this.generic}chess.com\`, or \`${this.generic}bughousetest\`.`;
+                if (this.command) data.msg = await this.Output.generic("Updating user " + dbuser.username + "...");
+                for (let source of data.sources) for (let account of Object.keys(data.dbuser[data.source.key])) if (!account.startsWith("_")) {
+                    if (data.command) data.edit();
+                    await data.setSource(source).setUsername(account).getData();
+                    data.setUsername();
+                }
+            } catch (e) {
+                if (e) data.setError(e);
+            }
 		} catch (e) {
 			if (e && this.command) this.Output.onError(e);
 		}
+    }
+    
+    /**
+     * Grabs the least up to date user in the database and runs them through the update process, without outputting a result
+     * @private
+     */
+	async updateCycle () {
+		let dbuser = this.nextUpdate();
+        Tracker.lastDBuser = dbuser;
+		if (!dbuser) return this.error("All users are up to date.");
+        this.update(dbuser);
     }
     
     async updateAll() {
@@ -215,6 +267,7 @@ class Tracker extends Parse {
             let arr = Array.from(accounts.keys());
             let arrs = [];
             while (ids.length > 0) {
+                
                 arrs = arrs.concat(arr.splice(0, 48));
             }
             let DataStore = new Map();
@@ -223,8 +276,9 @@ class Tracker extends Parse {
                 let ids = arrs[i];
                 let users = await Lichess.users.get(ids);
                 users.tap(async (id, parsedData) => {
-                    let data = DataStore.get(id) || new Track({dbuser});
+                    let data = DataStore.get(id) || new Track(this, DBuser.byID(id));
                     data.assign(parsedData);
+                    DataStore.set(id, dbuser);  
                 })
                 await this.Output.editor({  description: `Updated data on ${Math.max(48 * (i + 1), arr.length)} / ${arr.length}`}, msg);
                 await timeout(3000);
@@ -243,27 +297,12 @@ class Tracker extends Parse {
             let user = this.Search.users.byID(dbuser.id);
             if (!user) return dbuser.left = true;	
 			if (!/online|idle|dnd/.test(user.presence.status)) return false;
-			if (!dbuser.lastupdate) return true;
-			if (dbuser.lastupdate < currentValue && dbuser.lastupdate < Date.now() - config.delays.repeat) {
-				currentValue = dbuser.lastupdate;
+			if (!dbuser.lastUpdate) return true;
+			if (dbuser.lastUpdate < currentValue && dbuser.lastUpdate < Date.now() - config.delays.repeat) {
+				currentValue = dbuser.lastUpdate;
 				return dbuser;
 			}
         });      
-	}
-
-    /**
-     * 
-     * @param {TrackData} data 
-     */
-	static async handle(data) {
-		try {
-			let sourceData = /lichess|bughousetest/.test(data.source.key) ? await Lichess.users.get(data.username) : await new require('chesscom.js')(await Tracker.request(data), data);
-			if (!sourceData) throw data.setError('No responce received');
-            data.assign(parsedData).setSuccess();
-			return data;
-		} catch (e) {
-			throw e;
-		}
 	}
 
     /**
