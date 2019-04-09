3c8d3f79e32989f1510a9655ef81b19274da5165
theLAZYmd <lazymeep+github@gmail.com>
2019-04-09 18:36:47 +0100

    resolved conflicts

--- src/router/guildMemberAdd.js
+++ src/router/guildMemberAdd.js
@@ -1,22 +1,18 @@
-const settings = require("../settings");
-const Logger = require("../util/logger");
+const settings = require('../settings');
+const Logger = require('../util/logger');
 
 module.exports = async (client, member) => {
-<<<<<<< HEAD
-    try {
-        let foundRole = member.guild.roles.find(item => item.name === settings.unrankedRoleName);
-        if (foundRole) member.addRole(foundRole);
-    } catch (e) {
-        if (e) Logger.error(e);
-    }
-}
-=======
-	Logger.log(["auto", "guildMemberAdd", "join", "[" + member.user.tag + "]"]);
-	let Constructor = require("../modules/Administration/shadowban.js");
-	let Instance = new Constructor({ member })
+	try {
+		let foundRole = member.guild.roles.find(item => item.name === settings.unrankedRoleName);
+		if (foundRole) member.addRole(foundRole);
+	} catch (e) {
+		if (e) Logger.error(e);
+	}
+	Logger.log(['auto', 'guildMemberAdd', 'join', '[' + member.user.tag + ']']);
+	let Constructor = require('../modules/Administration/shadowban.js');
+	let Instance = new Constructor({ member });
 	Instance.sbusername(member);
 	let dbuser = DBuser.getUser(member.user);
 	if (dbuser.left) delete dbuser.left;
 	DBuser.setData(dbuser);
-}
->>>>>>> 1.8.0
+};
