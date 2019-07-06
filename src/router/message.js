14761f2e3061bcbe46a45b066094856bf4f24c8d
theLAZYmd <lazymeep+github@gmail.com>
2019-07-06 01:09:49 +0100

    Fixed bug with allMessageCommands not running on '' messages

--- src/router/message.js
+++ src/router/message.js
@@ -109,8 +109,8 @@ module.exports = async (client, message) => {
 				if (cmdInfo) await Message.run(argsInfo, cmdInfo);
 				return;
 			}
-			if (!/[a-z]/i.test(message.content)) throw '';
 			if (!argsInfo.message.guild) {
+				if (!/[a-z]/i.test(message.content)) throw '';
 				let cmdInfo = await Command.dm(argsInfo.command);
 				if (cmdInfo.guild) {
 					argsInfo.message._guild = await Message.setGuild(argsInfo, cmdInfo.guild); //now passed, just check if it needs a guild
@@ -119,6 +119,7 @@ module.exports = async (client, message) => {
 				return await Message.run(argsInfo, cmdInfo);
 			} else {
 				Command.all(argsInfo);
+				if (!/[a-z]/i.test(message.content)) throw '';
 				let cmdInfo = await Command.command(argsInfo.command);
 				if (cmdInfo) {
 					cmdInfo.command = true;
