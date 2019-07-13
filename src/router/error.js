0a030c74b63e38b4720760a14ba54ba0aeea066c
theLAZYmd <lazymeep+github@gmail.com>
2019-07-13 20:47:28 +0100

    Started just logging error events to see if that helps

--- src/router/error.js
+++ src/router/error.js
@@ -3,12 +3,12 @@ const Logger = require('../util/logger');
 module.exports = async (client, e) => {
 	try {
 		if (e && e.message) Logger.error(e.message);
-		await Logger.verbose('Destroying client...');
-		await client.destroy();
-		await Logger.verbose('Client successfully destroyed!');
-		await Logger.verbose('Re-establishing a webconnection...');
-		await client.login(require('../token.json').token);
-		await Logger.verbose('Webconnection successfully established!');
+		//await Logger.verbose('Destroying client...');
+		//await client.destroy();
+		//await Logger.verbose('Client successfully destroyed!');
+		//await Logger.verbose('Re-establishing a webconnection...');
+		//await client.login(require('../token.json').token);
+		//await Logger.verbose('Webconnection successfully established!');
 	} catch (e) {
 		if (e) Logger.error(e);
 	}
