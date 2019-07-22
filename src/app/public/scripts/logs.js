d143d9b23b17869e72066e8e98545d84c0ee59af
theLAZYmd <lazymeep+github@gmail.com>
2019-07-22 23:49:51 +0100

    Added click events to the sliders to function on mobile

--- src/app/public/scripts/logs.js
+++ src/app/public/scripts/logs.js
@@ -1,3 +1,54 @@
+let container = document.getElementById('container');
+let log_box = document.getElementById('ll0');
+let log_link1 = document.getElementById('ll1');
+let log_link2 = document.getElementById('ll2');
+let expando = false;
+
+container.onmouseover = function() {
+	transition_right(log_link1);
+	transition_right(log_link2);
+};
+
+container.onmouseleave = function() {
+	transition_left(log_link1);
+	transition_left(log_link2);
+};
+
+log_box.onclick = function() {
+	if (!expando) container.onmouseover();
+	else container.onmouseleave();
+};
+
+let link1 = document.createElement('a');
+link1.href = '/logs/debug.log';
+link1.target = 'blank';
+let link2 = document.createElement('a');
+link2.target = 'blank';
+link2.href = '/logs/error.log';
+
+log_link1.onclick = function() {
+	link1.click();
+};
+
+log_link2.onclick = function() {
+	link2.click();
+};
+
+function transition_right(element) {
+	element.style.width = '100%';
+	element.style.left = 100;
+	element.style.overflow = 'visible';
+	expando = true;
+}
+
+function transition_left(element) {
+	element.style.width = 0;
+	element.style.left = '100%';
+	element.style.overflow = 'hidden';
+	expando = false;
+}
+
+/*
 const logs = ['data', 'debug', 'error'];
 
 try {
@@ -30,3 +81,4 @@ function readTextFile(file) {
 	}
 	rawFile.send(null);
 }
+*/
