42de5753fe56c0e487b50560552705f8a86ecc1b
theLAZYmd <lazymeep+github@gmail.com>
2019-08-06 19:41:56 +0100

    Force modmail dir rename 2

---
+++ src/modules/ModMail/action.js
@@ -0,0 +1,154 @@
+const Main = require('./main.js');
+const Embed = require('../../util/embed');
+const Logger = require('../../util/logger');
+
+class Action extends Main {
+
+	constructor(message) {
+		super(message);
+	}
+
+	get output () {
+		let outputConstructor = require('./output.js');
+		return new outputConstructor(this.message);
+	}
+
+	/*data object {
+	  "mod": false if it's a new user mail. Otherwise it's the user object of the mod who sent the message {
+		"flair": true if new mail was sent with -s flag, or message was reacted with 🗣 emoji
+	  },
+	  "user": the user object of the conversation,
+	  "name": appears for mod actions, outputted to name fields
+	  "content": the last line of the conversation, to be processed
+	  "message": the conversation. Exists when the new message is a reply and history is found.
+	  "embed": message.embeds[0]. The embed to be sent.
+	}*/
+
+	async react(reaction, user) {
+		try {
+			let data = {
+				mod: user,
+				message: reaction.message,
+				embed: new Embed(reaction.message.embeds[0])
+			};
+			data.user = this.Search.users.byTag(this.modmail[reaction.message.id].tag);
+			if (!data.user) throw 'User **' + user.tag + '** no longer exists!';
+			data.mod.flair = false;
+			switch (reaction.emoji.name) {
+				case '❎':
+					this.close(data);
+					break;
+				case '🗣':
+					data.mod.flair = true;
+					this.reply(data);
+					break;
+				case '👤':
+					data.mod.flair = false;
+					this.reply(data);
+					break;
+				case '👁': //"seen"
+					return; //"Don't remove the emoji"
+				case '❗':
+					this.warn(data);
+					break;
+				case '⏲':
+					this.timeout(data);
+					break;
+			}
+			reaction.remove(user);
+		} catch (e) {
+			if (e) this.Output.onError(e);
+		}
+	}
+
+	async reply(data) {
+		try {
+			let msg = await this.Output.response({
+				author: data.mod,
+				description: '**' + data.mod.tag + '** Please type your response below (replying as ' + (data.mod.flair ? 'server' : 'yourself') + ')'
+			}, true);
+			let content = msg.content + ' ' + msg.attachments.map(([,a]) => '[Attachment](' + a.url + ')').join(' ');
+			if (content.length > 1024) throw 'Your message must be less than 1024 characters!\nPlease shorten it by **' + (content.length - 1024) + '** characters.';
+			else data.content = content; //DATA.CONTENT SET
+			msg.delete();
+			data.command = 'reply';
+			this.output.amend(data);
+			this.output.send(data);
+			Logger.data(data);
+		} catch (e) {
+			if (e) this.Output.onError(e);
+		}
+	}
+
+	async close(data) {
+		try {
+			await this.Output.confirm({
+				action: 'closing conversation for user **' + data.user.tag + '**',
+				author: data.mod
+			});
+			for (let id of Object.keys(this.modmail)) {
+				if (this.modmail[id].tag === data.user.tag) {
+					try {
+						let msg = await this.mchannel.fetchMessage(id);
+						msg.delete();
+					} catch (e) {
+						return '';
+					}
+					delete this.modmail[id];
+				}
+			}
+			this.setData(this.modmail);
+			this.Output.generic('**' + data.mod.tag + '** closed the ModMail conversation for **' + data.user.tag + '**.');
+			data.command = 'close';
+			Logger.data(data);
+		} catch (e) {
+			if (e) this.Output.onError(e);
+		}
+	}
+
+	async warn(data) {
+		try {
+			await this.Output.confirm({
+				action: 'warning for user **' + data.user.tag + '**',
+				author: data.mod
+			});
+			Object.assign(data, {
+				title: 'Warning from server ' + this.guild.name + ':',
+				content: 'You are abusing server modmail. Please be polite and do not spam the inbox.',
+				command: 'warn',
+				name: 'On ' + Date.getISOtime(Date.now()) + ', ' + data.mod.tag + ' warned user.'
+			});
+			this.output.moderate(data);
+			this.output.send(data);
+			Logger.data(data);
+		} catch (e) {
+			if (e) this.Output.onError(e);
+		}
+	}
+
+	async timeout(data) {
+		try {
+			await this.Output.confirm({
+				action: 'timeout for user **' + data.user.tag + '**',
+				author: data.mod
+			});
+			Object.assign(data, {
+				title: 'You have been timed out from sending messages to server ' + this.guild.name + ':',
+				content: 'The mod team will not receive your messages for 24 hours.',
+				command: 'timeout',
+				name: 'On ' + Date.getISOtime(Date.now()) + ', ' + data.mod.tag + ' timed out user for 24h.'
+			});
+			if (!this.modmail._timeout) this.modmail._timeout = {};
+			this.modmail._timeout[data.user.tag] = Date.now();
+			this.setData(this.modmail);
+			this.output.moderate(data);
+			this.output.send(data);
+			Logger.data(data);
+		} catch (e) {
+			if (e) this.Output.onError(e);
+		}
+	}
+
+}
+
+module.exports = Action;
---
+++ src/modules/ModMail/main.js
@@ -0,0 +1,95 @@
+const Parse = require('../../util/parse.js');
+const Embed = require('../../util/embed.js');
+const DataManager = require('../../util/datamanager.js');
+const Logger = require('../../util/logger.js');
+
+class ModMail extends Parse {
+	constructor(message) {
+		super(message);
+		this.mchannel = this.server ? this.Search.channels.get(this.server.channels.modmail) : null;
+	}
+    
+	get input() {
+		if (this._input) return this._input;
+		let Constructor = require('./input');
+		return this._input = new Constructor(this.message);
+	}
+
+	get output() {
+		if (this._output) return this._output;
+		let Constructor = require('./output');
+		return this._output = new Constructor(this.message);
+	}
+
+	get action() {
+		if (this._action) return this._action;
+		let Constructor = require('./action');
+		return this._action = new Constructor(this.message);
+	}
+
+	get modmail () {
+		let reactionmessages = this.reactionmessages;
+		return reactionmessages.modmail || {};
+	}
+
+	set modmail (modmail) {
+		this.reactionmessages.modmail = modmail;
+		DataManager.setServer(this.reactionmessages, './src/data/reactionmessages.json');
+	}
+
+	setData(modmail) {
+		this.reactionmessages.modmail = modmail;
+		DataManager.setServer(this.reactionmessages, './src/data/reactionmessages.json');
+	}
+
+	async log(data) {
+		try {
+			let args = [];
+			if (data.content) args.unshift(data.content);
+			if (data.mod && data.user) args.unshift(data.user.tag);
+			if (data.mod && data.users) args.unshift(...data.users.map(user => user.tag));
+			if (data.mod && /reply|send/.test(data.command)) args.unshift(data.mod.flair ? 'server' : 'self');
+			Logger.command(['Mod Mail', data.mod || data.user, data.command, '[' + args.join(', ') + ']']);
+		} catch (e) {
+			if (e) this.Output.onError('log ' + e);
+		}
+	}
+
+	async sort(data) {  //find the relevant modmil
+		try {
+			let f = Object.entries(this.modmail).find(([i, m]) => {
+				if (i.startsWith('_')) return false;
+				if (m.tag !== data.user.tag) return false;
+				if (m.overflow) return false;
+				return true;
+			});
+			if (!f) return await this.output.anew(data);
+			let [id, mailInfo] = f;
+			let modmail = await this.mchannel.fetchMessage(id)
+				.catch(async () => {
+					let user = data.mod || data.user;
+					await this.Output.confirm({ //if couldn't find the message
+						channel: this.mchannel,
+						description: '**' + user.tag + '** Couldn\'t find message. Please confirm that message no longer exists.',
+						role: 'admin'
+					});
+					delete this.modmail[id]; //and delete the record
+					this.setData(this.modmail);
+					await this.output.anew(data); //and make a new post
+					throw '';
+				}); //so if they have a chat history, find it
+			data = Object.assign(data, {
+				message: modmail,
+				embed: new Embed(modmail.embeds[0])
+			});
+			if (Date.now() - mailInfo.lastMail < 1800000 && !data.mod && data.embed.fields[data.embed.fields.length - 1].name.includes('user wrote:')) await this.output.append(data);
+			else await this.output.renew(data);
+		} catch (e) {
+			if (e) this.Output.onError(e);
+			throw e;
+		}
+	}
+
+}
+
+module.exports = ModMail;
