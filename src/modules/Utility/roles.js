8b61823c0e4d66960cf5c653aa7a592c200e9139
theLAZYmd <lazymeep+github@gmail.com>
2019-08-13 00:46:43 +0100

    [feature] added role self-assignment by group number

--- src/modules/Utility/roles.js
+++ src/modules/Utility/roles.js
@@ -60,17 +60,18 @@ class Roles extends Parse {
 	get(group) {
 		let server = this.server;
 		if (!server.sars) server.sars = [];
-		let embed = new Embed()
-			.setTitle('Self-assignable roles on ' + this.guild.name);			
+		if (!server.tars) server.tars = [true];
+		let embed = new Embed().setTitle('Self-assignable roles on ' + this.guild.name);			
 		//	.setTitle('Self-assignable roles on ' + this.guild.name + (group !== undefined ? ', for Group ' + group : ''));
 		//if (group !== undefined) server.sars = server.sars.map((r, i) => i === group ? r : []);
 		for (let i = 0; i < server.sars.length; i++) {
 			let key = '', value = '';
+			if (server.tars[i] === undefined || server.tars[i] === null) server.tars[i] = false;
 			for (let id of server.sars[i]) {
 				let role = this.Search.roles.byID(id);
 				if (!role) continue;
 				value += role + '\n';
-				if (!key) key = i === 0 ? 'Group 0 [Non-exclusive]' : 'Group ' + i;
+				if (!key) key = 'Group ' + i + (server.tars[i] ? ' [Non-exclusive]' : '');
 			}
 			if (key) embed.addField(key, value, false);
 		}
@@ -83,6 +84,7 @@ class Roles extends Parse {
 		try {
 			let server = this.server;
 			if (!server.sars) server.sars = [];
+			if (!server.tars) server.tars = [true];
 
 			//Parse the values
 			if (isNaN(group)) group = 0;
@@ -106,6 +108,7 @@ class Roles extends Parse {
 			//Set the values
 			for (let i = 0; i <= group; i++) {
 				if (!server.sars[i]) server.sars[i] = [];
+				if (server.tars[i] === undefined || server.tars[i] === null) server.tars[i] = false;
 			}
 			for (let role of roles) {
 				if (server.sars.find(group => group.find(r => r === role.id))) throw 'Role ' + role.name + ' is already self-assignable';
@@ -121,6 +124,8 @@ class Roles extends Parse {
 	async rsar(argument = this.argument) {
 		try {
 			let server = this.server;
+			if (!server.sars) server.sars = [];
+			if (!server.tars) server.tars = [true];
 			if (!argument) argument = await this.Output.response({
 				description: 'Please specify a valid role or group number',
 				filter: (r) => {
@@ -133,7 +138,8 @@ class Roles extends Parse {
 			});
 			if (!isNaN(Number(argument))) {
 				if (!server.sars[argument]) throw new RangeError('Group number must be from 0 to '  + (server.sars.length - 1));
-				server.sars[argument] = [];	
+				server.sars[argument] = [];
+				server.tars[argument] = argument === 0;
 				this.server = server;			
 				this.get();
 			} else {
@@ -152,34 +158,89 @@ class Roles extends Parse {
 		}
 	}
 
-	async assign (argument = this.argument) {
+	async tesar(argument = this.argument) {
 		try {
-			//Parse the role
-			let role = this.Search.roles.get(argument);
-			if (!role) throw 'No such role!';
-
-			//Assigns the role
 			let server = this.server;
 			if (!server.sars) server.sars = [];
-			for (let group = 0; group < server.sars.length; group++) {
-				if (server.sars[group].includes(role.id)) {
-					if (this.member.roles.has(role.id)) {
-						if (this.command === 'iam') throw 'You already have role ' + role.name;
-						this.member.removeRole(role).catch(() => {});
-						this.Output.generic('You no longer have **' + role.name + '** role');
+			if (!server.tars) server.tars = [true];
+			if (!argument) argument = await this.Output.response({
+				description: 'Please specify a valid role or group number',
+				number: true,
+				filter: (r) => server.tars[r] !== undefined
+			});
+			if (isNaN(Number(argument)) || server.sars[argument] === undefined) throw new RangeError('Group number must be from 0 to '  + (server.sars.length - 1));
+			server.tars[argument] = !server.tars[argument];
+			this.Output.generic('Roles in group **' + argument + '** are ' + (server.tars[argument] ? 'now' : 'no longer') + ' non-exclusive');
+			this.server = server;
+		} catch (e) {
+			if (e) this.Output.onError(e);
+		}
+	}
+
+	async assign (argument = this.argument) {
+		try {
+			let server = this.server;
+			if (!server.sars) server.sars = [];
+			if (!server.tars) server.tars = [true];
+
+			//Argument can be a group number or a specific role
+			if (isNaN(Number(argument))) {
+				//Parse the role
+				let role = this.Search.roles.get(argument);
+				if (!role) throw 'No such role!';
+
+				//Assigns the role
+				for (let group = 0; group < server.sars.length; group++) {
+					if (server.tars[group] === undefined || server.tars[group] === null) server.tars[group] = false;
+					if (server.sars[group].includes(role.id)) {
+						if (this.member.roles.has(role.id)) {
+							if (this.command === 'iam') throw 'You already have role ' + role.name;
+							this.member.removeRole(role).catch(() => {});
+							this.Output.generic('You no longer have **' + role.name + '** role');
+						} else {
+							if (this.command === 'iamn') throw 'You do not have role ' + role.name;
+							this.member.addRole(role).catch(() => {});
+							this.Output.generic('You now have **' + role.name + '** role');
+							if (!server.tars[group]) for (let id of server.sars[group]) {
+								if (id === role.id) continue;
+								if (this.member.roles.has(id)) this.member.removeRole(id).catch(() => {});
+							}
+						}
+						return;
+					}
+				}
+				throw 'Role ' + role.name + ' is not self-assignable';
+			} else {
+				let group = argument;
+				if (group < 0 || group > Math.max(server.sars.length, 1)) throw new RangeError('Group must be within 0 to ' + Math.max(server.sars.length, 1) + ' range');
+				let add = (() => {
+					if (this.command === 'iam') return true;
+					if (this.command === 'iamn') return false;
+					return !server.sars[group].every(id => this.member.roles.has(id));
+				})();
+				if (add && !server.tars[group]) throw 'Can\'t assign roles by group-number for an exclusive group';
+				let list = [];
+				for (let id of server.sars[group]) {
+					let role = this.Search.roles.byID(id);
+					if (!role) continue;
+					if (add) {
+						if (!this.member.roles.has(id)) {
+							this.member.addRole(id);
+							list.push(role.name);
+						}
 					} else {
-						if (this.command === 'iamn') throw 'You do not have role ' + role.name;
-						this.member.addRole(role).catch(() => {});
-						this.Output.generic('You now have **' + role.name + '** role');
-						if (group !== 0) for (let id of server.sars[group]) {
-							if (id === role.id) continue;
-							if (this.member.roles.has(id)) this.member.removeRole(id).catch(() => {});
+						if (this.member.roles.has(id)) {
+							this.member.removeRole(id);
+							list.push(role.name);
 						}
 					}
-					return;
 				}
+				if (list.length) this.Output.sender(new Embed()
+					.setTitle((add ? 'Added' : 'Removed') + ' the following roles:')
+					.setDescription(list.join('\n'))
+				);
+				else this.Output.onError(`You ${add ? 'already' : 'don\'t'} have ${add ? 'all' : 'any of'} the roles in group ${group}`);
 			}
-			throw 'Role ' + role.name + ' is not self-assignable';
 		} catch (e) {
 			if (e) this.Output.onError(e);
 		}
