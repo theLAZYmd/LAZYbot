919e6aee12c28ee9472656dc22332bf4cb02f78c
theLAZYmd <lazymeep+github@gmail.com>
2019-08-14 20:03:22 +0100

    [bug] modmail helper message wasn't showing on reaction click

--- src/modules/ModMail/input.js
+++ src/modules/ModMail/input.js
@@ -1,6 +1,5 @@
 const Main = require('./main');
 const Embed = require('../../util/embed');
-const Logger = require('../../util/logger');
 
 class Input extends Main {
 
@@ -81,7 +80,8 @@ class Input extends Main {
 	async reaction() {
 		return this.Output.sender(new Embed()
 			.setTitle('On ' + Date.getISOtime(this.message.createdTimestamp) + ', you successfully sent to ModMail:')
-			.setDescription(this.message.content.format() + '\n\'📨\' constitutes a successful message sent. If your message did not receive a \'📨\' or you received an error message, please contact theLAZYmd#2353**.', false)
+			.setDescription(this.message.content.format())
+			.setFooter('📨\' constitutes a successful message sent. If your message did not receive a \'📨\' or you received an error message, please contact theLAZYmd#2353**.', false)
 		);
 	}
 
--- src/router/messageReactionAdd.js
+++ src/router/messageReactionAdd.js
@@ -1,51 +1,53 @@
-const DataManager = require("../util/datamanager.js");
-const Logger = require("../util/logger.js");
+const path = require('path');
+const DataManager = require('../util/datamanager.js');
+const Logger = require('../util/logger.js');
 
-const Commands = require("../util/commands.js");
-const {    name, key    } = Commands.reaction;
+const Commands = require('../util/commands.js');
+const {name, key} = Commands.reaction;
 
 class messageReactionAdd {
 
-    static async bot () {
+	static async bot () {
 
-    }
+	}
 
-    static async button (messageReaction, user) {
-        const reactionMessages = DataManager.getFile("./src/data/reactionmessages.json")[messageReaction.message.guild.id];
-        let array = [];
-        for (let [type, data] of Object.entries(reactionMessages)) {
-            if (type.startsWith("_")) continue;
-            array = array.concat(Object.entries(data).map(([k, v]) => [k, [v, type]]));
-        }
-        let ids = new Map(array);
-        let val = ids.get(messageReaction.message.id);
-        if (!val) return;
-        let [msgInfo, type] = val;
-        let cmdInfo = key.get(type);
-        let path = "../modules/" + (cmdInfo.module ? cmdInfo.module + "/" : "") + cmdInfo.file.toLowerCase() + ".js";
-        let Constructor = require(path);
-        let Instance = new Constructor(messageReaction.message);
-        if (typeof Instance[cmdInfo.method] !== "function") return Logger.error(path + " | " + cmdInfo.method + "() is not a function.")
-        Instance[cmdInfo.method](messageReaction, user, msgInfo);
-    }
+	static async button (messageReaction, user) {
+		const reactionMessages = DataManager.getFile('./src/data/reactionmessages.json')[messageReaction.message.guild.id];
+		let array = [];
+		for (let [type, data] of Object.entries(reactionMessages)) {
+			if (type.startsWith('_')) continue;
+			array = array.concat(Object.entries(data).map(([k, v]) => [k, [v, type]]));
+		}
+		let ids = new Map(array);
+		let val = ids.get(messageReaction.message.id);
+		if (!val) return;
+		let [msgInfo, type] = val;
+		let cmdInfo = key.get(type);
+		let path = '../modules/' + (cmdInfo.module ? cmdInfo.module + '/' : '') + cmdInfo.file.toLowerCase() + '.js';
+		let Constructor = require(path);
+		let Instance = new Constructor(messageReaction.message);
+		if (typeof Instance[cmdInfo.method] !== 'function') return Logger.error(path + ' | ' + cmdInfo.method + '() is not a function.');
+		Instance[cmdInfo.method](messageReaction, user, msgInfo);
+	}
 
-    static async name (messageReaction, user) {
-        let cmdInfo = name.get(messageReaction.name);
-        if (!cmdInfo) return null;
-        if (cmdInfo.active === false) return null;
-        let Constructor = require("../modules/" + cmdInfo.module + cmdInfo.file.toLowerCase() + ".js");
-        let Instance = new Constructor(messageReaction.message);
-        Instance[cmdInfo.method.toLowerCase()](messageReaction, user);
-    }
+	static async name (messageReaction, user) {
+		let cmdInfo = name.get(messageReaction.emoji.name);
+		if (!cmdInfo) return null;
+		if (cmdInfo.active === false) return null;
+		const file = path.join(process.cwd(), 'src', 'modules', cmdInfo.module, cmdInfo.file.toLowerCase());
+		let Constructor = require(file);
+		let Instance = new Constructor(messageReaction.message);
+		Instance[cmdInfo.method.toLowerCase()](messageReaction, user);
+	}
 
 }
 
 module.exports = async (client, messageReaction, user) => {
-    try {
-        if (user.bot) messageReactionAdd.bot(messageReaction, user);
-        else if (messageReaction.message.guild && messageReaction.message.author.bot) messageReactionAdd.button(messageReaction, user);
-        else messageReactionAdd.name(messageReaction, user);
-    } catch (e) {
-        if (e) Logger.error(e);
-    }
-}
+	try {
+		if (user.bot) messageReactionAdd.bot(messageReaction, user);
+		else if (messageReaction.message.guild && messageReaction.message.author.bot) messageReactionAdd.button(messageReaction, user);
+		else messageReactionAdd.name(messageReaction, user);
+	} catch (e) {
+		if (e) Logger.error(e);
+	}
+};
